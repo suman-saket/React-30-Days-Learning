c106a63a4204386191ad6d1591a0d8584590a2c2
Saket Suman <sumansaket7@gmail.com>
2021-03-02 20:05:01 +0530

    day-6 learning added

---
+++ Day-6/src/App.js
@@ -0,0 +1,13 @@
+import LtwentyFive from "./components/LtwentyFive";
+import Table from "./components/Table";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LtwentyFive /> */}
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ Day-6/src/components/LtwentyFive.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LtwentyFive() {
+  return (
+    <React.Fragment>
+      <h1>Fragment Demo</h1>
+      <p>This describes fragment demo component</p>
+    </React.Fragment>
+  );
+}
+
+export default LtwentyFive;
