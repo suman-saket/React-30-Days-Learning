c14efa562a3befa0e782526be5fc83a29a202d18
Saket Suman <sumansaket7@gmail.com>
2021-07-16 10:24:55 +0530

    useEffect with cleanup

---
+++ Day-14/src/components/MouseContainer.js
@@ -0,0 +1,17 @@
+//useEffect with cleanUp
+//this container will toggle component visibility and effectively unMount the component from the DOM.
+
+import React, { useState } from "react";
+import HookMouse from "./HookMouse";
+
+function MouseContainer() {
+  const [display, setDisplay] = useState(true);
+  return (
+    <div>
+      <button onClick={() => setDisplay(!display)}>Toggle display</button>
+      {display && <HookMouse />}
+    </div>
+  );
+}
+
+export default MouseContainer;
